d8f6e1f3d3beb319fec462f026a4b0ded3340f4a
Harshitha-Shivappa <56878541+Harshitha-Shivappa@users.noreply.github.com>
2021-04-14 20:29:55 +0530

    Updated the file with matchLocatorsAndFetchElement method

--- konakart/src/main/java/com/atmecs/konakart/helpers/GetElementFromLocators.java
+++ konakart/src/main/java/com/atmecs/konakart/helpers/GetElementFromLocators.java
@@ -1,5 +1,7 @@
 package com.atmecs.konakart.helpers;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -9,6 +11,7 @@ import org.openqa.selenium.WebElement;
  */
 public class GetElementFromLocators {
 	static WebElement element;
+	static List<WebElement> elements;
 
 	/**
 	 * method takes parameters as
@@ -16,43 +19,55 @@ public class GetElementFromLocators {
 	 * @param locators and match for the locators using its type
 	 * @return element
 	 */
-	public static WebElement matchLocatorsAndFetchElements(String locators,WebDriver driver) 
-	{
-		String locatorsArray[]=locators.split(";");
-		switch(locatorsArray[0].toLowerCase())
-		{
-			case "id":
-				element = driver.findElement(By.id(locatorsArray[1]));
-				break;
-				
-			case "name":
-				element = driver.findElement(By.name(locatorsArray[1]));
-				break;
-				
-			case "class":
-				element = driver.findElement(By.className(locatorsArray[1]));
-				break;
-			
-			case "linktext":
-				element = driver.findElement(By.linkText(locatorsArray[1]));
-				break;
-			
-			case "partialLink":
-				element = driver.findElement(By.partialLinkText(locatorsArray[1]));
-				break;
-				
-			case "tag":
-				element = driver.findElement(By.tagName(locatorsArray[1]));
-				break;
-			
-			case "css":
-				element = driver.findElement(By.cssSelector(locatorsArray[1]));
-				break;
-			
-			case "xpath":
-				element = driver.findElement(By.xpath(locatorsArray[1]));
-				break;		
+	public static WebElement matchLocatorsAndFetchElements(String locators, WebDriver driver) {
+		String locatorsArray[] = locators.split(";");
+		switch (locatorsArray[0].toLowerCase()) {
+		case "id":
+			element = driver.findElement(By.id(locatorsArray[1]));
+			break;
+
+		case "name":
+			element = driver.findElement(By.name(locatorsArray[1]));
+			break;
+
+		case "class":
+			element = driver.findElement(By.className(locatorsArray[1]));
+			break;
+
+		case "linktext":
+			element = driver.findElement(By.linkText(locatorsArray[1]));
+			break;
+
+		case "partialLink":
+			element = driver.findElement(By.partialLinkText(locatorsArray[1]));
+			break;
+
+		case "tag":
+			element = driver.findElement(By.tagName(locatorsArray[1]));
+			break;
+
+		case "css":
+			element = driver.findElement(By.cssSelector(locatorsArray[1]));
+			break;
+
+		case "xpath":
+			element = driver.findElement(By.xpath(locatorsArray[1]));
+			break;
+
 		}
-	    return element;
+		return element;
+	}
+
+	public static List<WebElement> matchLocatorsAndFetchElement(String locators, WebDriver driver) {
+		String locatorsArray[] = locators.split(";");
+		switch (locatorsArray[0].toLowerCase()) {
+		case "xpath":
+			elements = driver.findElements(By.xpath(locatorsArray[1]));
+			break;
+
+		default:
+			System.out.println("Invalid");
+		}
+		return elements;
 	}
 }
